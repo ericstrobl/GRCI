5c64406b623d08928dcc51b2b74ebc1b3f8b5c79
Eric V. Strobl <ericvonstrobl@gmail.com>
2022-05-12 14:36:16 -0500

    Delete RANN-master directory

--- RANN-master/R/RANN-package.R
+++
@@ -1,8 +0,0 @@
-#' Wrapper for Arya and Mount's Approximate Nearest Neighbours (ANN) C++ library
-#' 
-#' @name RANN-package
-#' @aliases RANN
-#' @seealso \code{\link{nn2}}
-#' @docType package
-#' @keywords package
-NULL
